5d201ad8d90d46101ee7465c2563d573f9f50965
Dcode202 <49726802+Dcode202@users.noreply.github.com>
2022-07-31 20:02:32 +0530

    T4 update

--- Tutorial 4/Q3.scala
+++ Tutorial 4/Q3.scala
@@ -12,5 +12,5 @@ object Q3 extends App{
   println(formatNames("Benny", toUpper(_)))
   println(formatNames("Niroshan".substring(0,2), toUpper(_)) + formatNames("Niroshan".substring(2), toLower(_)))
   println(formatNames("Saman", toLower(_)))
-  println(formatNames("Kumara".substring(0,5), toLower(_)) + formatNames("Kumara".substring(5), toUpper(_)))
+  println(formatNames("Kumara".substring(0,1), toUpper(_)) + formatNames("Kumara".substring(1,5), toLower(_)) + formatNames("Kumara".substring(5), toUpper(_)))
 }
